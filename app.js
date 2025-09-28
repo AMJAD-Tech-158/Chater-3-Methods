ac471b69415960721c90a0025da463add48a06d9
AMJAD ALI <amjadalitech158@gmail.com>
2025-09-29 04:06:57 +0530

    creating an array

--- app.js
+++ app.js
@@ -21,4 +21,20 @@ console.log(`The first index of : "${searchTerm}" is ${indexofFirst}.`);
 const indexofSecond = paragraph.indexOf(searchTerm , indexofFirst+1);
 console.log(`The indexof second : "${searchTerm}" is ${indexofSecond}`);
 
+//slice methods in string
+const str2 = "The quick brown fox jumps over the lazy dog.";
+console.log(str2.slice(31));
+console.log(str2.slice(4,19));
+console.log(str2.slice(-4));
+console.log(str2.slice(-9,-5));
+//\ replace methods
+let sss = "Amjad is Good Boy.";
+console.log(sss.replace('Good','Bad'));
+//repeate methods
+let rrr = "AMJADG";
+console.log(rrr.repeat(3));
 //
+let arr = ['Amjad','Asgar','Zamshed','Aamina'];
+let arrMix = ['Amjad',322,true,4.4];
+let marks = [12,22,32,213,3432];
+let emp = [];
