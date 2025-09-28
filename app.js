2e3f478e80ff70ad2129fa309c8dc2bb6beb2c92
AMJAD ALI <amjadalitech158@gmail.com>
2025-09-29 02:23:48 +0530

    add first js file & first methods that's anme is trim methods

---
+++ app.js
@@ -0,0 +1,10 @@
+// 1.) Trim Methods
+const greeting = " Hello Dear from IGNOU      ";
+const trmiMethods = greeting.trim();
+console.log(trmiMethods);
+//This methods are both side in remove empty spaces.
+const trimEndMethods = greeting.trim();
+console.log(trimEndMethods);
+//
+const trimStartMethods = greeting.trimStart();
+console.log(greeting.trimStart());
