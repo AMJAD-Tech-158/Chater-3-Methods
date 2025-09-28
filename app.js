84c64d4c4bddcd62d025dfa05e0f9d0de3761eae
AMJAD ALI <amjadalitech158@gmail.com>
2025-09-29 03:33:30 +0530

    add indexof based one 1st question

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ console.log(trimEndMethods);
 //
 const trimStartMethods = greeting.trimStart();
 console.log(greeting.trimStart());
+//NO EXERCISE
+//
+let str = "ILoveCoding";
+console.log(str.indexOf("ing"));
+//indexof() === string first occurence
+const paragraph = "I think Ruth's dog is cutter than your dog!";
+const searchTerm = "dog";
+const indexofFirst = paragraph.indexOf(searchTerm);
+console.log(`The first index of : "${searchTerm}" is ${indexofFirst}.`);
+
+const indexofSecond = paragraph.indexOf(searchTerm , indexofFirst+1);
+console.log(`The indexof second : "${searchTerm}" is ${indexofSecond}`);
+
+//
